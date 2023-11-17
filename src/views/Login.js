1e502e3ce7c42caca2a35d3e008b9ad23f87d801
hitruong0504 <hitruong890@gmail.com>
2023-11-17 18:02:16 +0700

    update login and registration form for BE

--- src/views/Login.js
+++ src/views/Login.js
@@ -14,7 +14,25 @@ const Login = () =>{
         navigate('/register');
     };
 
-    const handleHome = () => {
+    const handleHome = async () => {
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        const response = await fetch('http://localhost:8080/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password }),
+        });
+
+        if (response.ok) {
+            console.log('Login successful');
+            // Redirect or set state to indicate successful login
+        } else {
+            console.error('Invalid credentials');
+            // Handle failed login: show error message or perform necessary action
+        }
         if (isCaptchaVerified) {
             // Perform login action
             navigate('/');
--- src/views/Register.js
+++ src/views/Register.js
@@ -2,12 +2,49 @@ import "./Register.css"
 import Form from 'react-bootstrap/Form';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useNavigate } from "react-router-dom";
-
+import axios from 'axios';
 
 
 const Register = () =>{
 
     const navigate = useNavigate();
+    const handleRegister = async (event) => {
+        event.preventDefault();
+
+        const name = event.target.elements.name.value;
+        const username = event.target.elements.username.value;
+        const birthdate = event.target.elements.birthdate.value;
+        const password = event.target.elements.password.value;
+        const email = event.target.elements.email.value;
+
+        const registrationData = {
+            name,
+            username,
+            birthdate,
+            password,
+            email,
+        };
+
+        try {
+            const response = await axios.post('http://localhost:8080/api/register', registrationData, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (response.status === 200) {
+                navigate("/login");
+            } else {
+                console.error(response.data); // Log server error message
+
+                if (response.status === 400 && response.data === 'Username already exists') {
+                    alert('Username already exists. Please choose a different username.');
+                }
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     const handleLogin = () => {
       navigate("/login");
@@ -17,12 +54,12 @@ const Register = () =>{
         <>
             <h2 className="animate-charcter">Register</h2> 
             <div className="form-login">
-                <Form className="login-form">
+                <Form className="login-form" onSubmit={handleRegister}>
                     <div className="input-group">
                         <div className="input-row">
                             <div className="input-item">
-                                <label for="username">Họ và tên</label>
-                                <input type="text" id="username" name="username" required/>
+                                <label for="name">Họ và tên</label>
+                                <input type="text" id="name" name="name" required/>
                             </div>
                             <div className="input-item">
                                 <label htmlFor="birthdate">Ngày sinh</label>
@@ -41,8 +78,8 @@ const Register = () =>{
                                 <input type="password" id="password" name="password" required/>
                             </div>
                             <div className="input-item">
-                                <label for="password">Nhập lại mật khẩu</label>
-                                <input type="password" id="password" name="password" required/>
+                                <label for="pwd">Nhập lại mật khẩu</label>
+                                <input type="password" id="pwd" name="pwd" required/>
                             </div>
                         </div>
                     </div>
